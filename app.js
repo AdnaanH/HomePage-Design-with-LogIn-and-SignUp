ec027974c9f901ad96aa420c3c9841f057200cc2
AdnaanH <60857004+AdnaanH@users.noreply.github.com>
2023-05-26 11:05:52 +0300

    Complete Responsiveness and Javascript Code

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById("su_page").addEventListener("click", function(){
+    document.getElementById("sign_up").style.display = "grid";
+})
+
+document.getElementById("close").addEventListener("click", function(){
+    document.getElementById("sign_up").style.display = "none";
+})
